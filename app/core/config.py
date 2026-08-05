5cb63f5a7a31fe8b6ce3ef9151509b6dc9648db1
Lftobs <tobbytobs1@gmail.com>
2026-08-05 02:45:10 +0100

    chore: update environment example template.

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 APP_ENV = os.getenv("APP_ENV", "staging")
 
 ENV_FILES = {
+	"dev": ".env",
 	"staging": ".env.staging",
 	"production": ".env.production",
 }
